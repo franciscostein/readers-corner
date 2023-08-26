e1eadfe117d2ce375f0a0ff4eac292d6107fc2e1
Leandro Costa <leandro.franc2008@hotmail.com>
2023-08-26 11:01:47 -0300

    solved AutoMapper error

--- servers/core/Program.cs
+++ servers/core/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using ReadersCorner.Core.Repositories;
 using ReadersCorner.Core.Repositories.Configurations;
 using ReadersCorner.Core.Repositories.Interfaces;
+using ReadersCorner.Core.Services;
+using ReadersCorner.Core.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +15,12 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.EnableSensitiveDataLogging();
 });
 builder.Services.AddControllers();
+builder.Services.AddScoped<IAuthorService, AuthorService>();    
+builder.Services.AddScoped<IBookService, BookService>();    
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
