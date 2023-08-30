96bc50cd6c761a0280e00b615cd78925c68aa972
Leandro Costa <leandro.franc2008@hotmail.com>
2023-08-30 10:58:01 -0300

    user's controller and service scafolding

---
+++ servers/core/Services/UserService.cs
@@ -0,0 +1,33 @@
+using ReadersCorner.Core.Models;
+using ReadersCorner.Core.Services.Interfaces;
+
+namespace ReadersCorner.Core.Services
+{
+    public class UserService : IUserService
+    {
+        public User Add(User model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
